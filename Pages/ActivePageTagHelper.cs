a63804b144576492463605d515cba00b20dd5150
maso1905 <maso1905@student.miun.se>
2021-01-25 11:51:52 +0100

    Second commit

---
+++ Pages/ActivePageTagHelper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace moment1_razorpage.Pages.TagHelpers
+{
+    [HtmlTargetElement(Attributes = "is-active-page")]
+    public class ActivePageTagHelper : TagHelper
+    {
+        [HtmlAttributeName("asp-page")]
+        public string Page { get; set; }
+
+        [HtmlAttributeNotBound]
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            base.Process(context, output);
+
+            if (ShouldBeActive())
+            {
+                MakeActive(output);
+            }
+
+            output.Attributes.RemoveAll("is-active-page");
+        }
+
+        private bool ShouldBeActive()
+        {
+            string currentPage = ViewContext.RouteData.Values["Page"].ToString();
+
+            if (!string.IsNullOrWhiteSpace(Page) && Page.ToLower() != currentPage.ToLower())
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private void MakeActive(TagHelperOutput output)
+        {
+            var classAttr = output.Attributes.FirstOrDefault(a => a.Name == "class");
+            if (classAttr == null)
+            {
+                classAttr = new TagHelperAttribute("class", "active");
+                output.Attributes.Add(classAttr);
+            }
+            else if (classAttr.Value == null || classAttr.Value.ToString().IndexOf("active") < 0)
+            {
+                output.Attributes.SetAttribute("class", classAttr.Value == null
+                    ? "active"
+                    : classAttr.Value.ToString() + " active");
+            }
+        }
+    }
+}
